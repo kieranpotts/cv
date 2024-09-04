54360217e3c8bc35090e075f9a1f892a4783a7f2
Kieran Potts <hello@kieranpotts.com>
2024-09-04 08:08:46 +0100

    feature: add docker build script

---
+++ run/pdf.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+# Pass additional CLI parameters as attributes to Asciidoctor via its `-a`
+# option. Various `env-*` attributes can be used to conditionally toggle parts
+# of the document.
+# https://docs.asciidoctor.org/asciidoc/latest/attributes/document-attributes-ref/
+attributes="${@}"
+if [ -z "${attributes}" ]; then
+  attributes='env-default'
+fi
+
+# The directory where the source files are located, relative to the project root.
+SOURCE_DIR=src
+
+# The directory where the generated output files will be created, relative to
+# the project root
+BUILD_DIR=dist
+
+docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf \
+  -a $attributes \
+  -D $BUILD_DIR \
+  -o "Kieran Potts - CV.pdf" \
+  $SOURCE_DIR/cv.adoc
+
+docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf \
+  -a $attributes \
+  -D $BUILD_DIR \
+  -o "Kieran Potts - Cover Letter.pdf" \
+  $SOURCE_DIR/letter.adoc
+
