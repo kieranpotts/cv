59a36114a06993754b6ef84041212493b95e6787
Kieran Potts <hello@kieranpotts.com>
2024-09-30 13:42:45 +0100

    maintenance: allow passing of multiple attributes to asciidoctor-pdf

--- run/pdf.sh
+++ run/pdf.sh
@@ -6,7 +6,7 @@
 # https://docs.asciidoctor.org/asciidoc/latest/attributes/document-attributes-ref/
 attributes="${@}"
 if [ -z "${attributes}" ]; then
-  attributes='env-default'
+  attributes=()
 fi
 
 # The directory where the source files are located, relative to the project root.
@@ -17,13 +17,13 @@ SOURCE_DIR=src
 BUILD_DIR=dist
 
 docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf \
-  -a $attributes \
+  $(for attr in $attributes; do echo -a $attr; done) \
   -D $BUILD_DIR \
   -o "Kieran Potts - CV.pdf" \
   $SOURCE_DIR/cv.adoc
 
 docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf \
-  -a $attributes \
+  $(for attr in $attributes; do echo -a $attr; done) \
   -D $BUILD_DIR \
   -o "Kieran Potts - Cover Letter.pdf" \
   $SOURCE_DIR/letter.adoc
