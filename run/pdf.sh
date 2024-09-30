971c8410f761f0769d7c9b013c28fc7dd8d4ed24
Kieran Potts <hello@kieranpotts.com>
2024-09-30 14:52:17 +0100

    feature: merge files using cpdf

--- run/pdf.sh
+++ run/pdf.sh
@@ -16,15 +16,41 @@ SOURCE_DIR=src
 # the project root
 BUILD_DIR=dist
 
+# Generate the CV PDF file.
 docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf \
   $(for attr in $attributes; do echo -a $attr; done) \
   -D $BUILD_DIR \
   -o "Kieran Potts - CV.pdf" \
   $SOURCE_DIR/cv.adoc
 
+# Generate the cover letter PDF.
 docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf \
   $(for attr in $attributes; do echo -a $attr; done) \
   -D $BUILD_DIR \
   -o "Kieran Potts - Cover Letter.pdf" \
   $SOURCE_DIR/letter.adoc
+
+# The above commands use a temporary container to generate the initial PDF
+# files. We want to modify these files using the `cpdf` tool, but Docker-generated
+# files will be owned by root and other users will have only read access. To
+# allow editing, change ownership to the current user.
+me=$(whoami)
+sudo chown $me:$me "$BUILD_DIR/Kieran Potts - CV.pdf"
+sudo chown $me:$me "$BUILD_DIR/Kieran Potts - Cover Letter.pdf"
+
+# Use cpdf to make some adjustments to the generated PDF files, eg. give them
+# titles (which are not created by asciidoctor-pdf), and also merge them into
+# a single file. This tool sends to stdout an annoying license message, which
+# I'm redirecting away. https://github.com/coherentgraphics/cpdf-binaries/
+./bin/cpdf "$BUILD_DIR/Kieran Potts - CV.pdf" \
+  AND -set-title "Kieran Potts - CV" \
+  -o "$BUILD_DIR/Kieran Potts - CV.pdf" &> /dev/null
+
+./bin/cpdf "$BUILD_DIR/Kieran Potts - Cover Letter.pdf" \
+  AND -set-title "Kieran Potts - Cover Letter" \
+  -o "$BUILD_DIR/Kieran Potts - Cover Letter.pdf" &> /dev/null
+
+./bin/cpdf -merge $BUILD_DIR/"Kieran Potts - Cover Letter.pdf" $BUILD_DIR/"Kieran Potts - CV.pdf" \
+  AND -set-title "Kieran Potts - CV and Cover Letter" \
+  -o $BUILD_DIR/"Kieran Potts - CV and Cover Letter.pdf" &> /dev/null
 
